f22bf3aba7b020e011aa1621f8d912d271f53b3e
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2022-04-04 21:13:39 -0500

    use cart logic

---
+++ README.md
@@ -0,0 +1,14 @@
+## My notes
+
+* simple-cart `(Goncy Challenges for Github)`
+  * source [Goncy Challenges](https://github.com/goncy/interview-challenges/tree/main/simple-cart)
+  * `(Twitch streaming, YOUTUBE IS PENDING)`
+
+* Im using ChakraUI for styles
+* The main object from this project for me is how use data (products) and ...
+* ... add product buton that use a counter
+
+* Pending `(How ignore a file correctly)`
+
+
+# Vite Project
--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,80 @@ import './App.css'
 function App() {
 
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([])
+
+  const handleAdd = (product) => {
+    const isInCart = cart.some(item => item.product.id === product.id)
+
+    if(isInCart) {
+      setCart((cart) => 
+        cart.map((item) => 
+          item.product.id === product.id ? {...item, count: item.count + 1} : item 
+        )
+      )
+    } else {
+      setCart((cart) => 
+        cart.concat({
+          product,
+          count: 1
+        })
+      )
+    }
+  }
+
+  const handleIncrement = (productId) => {
+    setCart((cart) => 
+      cart.map((item) => 
+        item.product.id === productId ? {...item, count: item.count + 1} : item 
+      )
+    )
+  }
+
+  const handleDecrement = (productId) => {
+    const cartItem = cart.find(item => item.product.id === productId)
+    if (cartItem.count === 1) {
+      console.log("/// Pendiente");
+    } else {
+      setCart((cart) => 
+        cart.map((item) => 
+          item.product.id === productId ? {...item, count: item.count - 1} : item 
+        )
+      )
+    }
+  }
 
   useEffect(() => {
     api.list().then(setProducts);
   }, []);
 
+  console.log("///cart:", cart);
+
   return (
     <Container maxWidth="container.xl">
       <Flex borderBottomWidth={1} fontSize="2xl" fontWeight="bold" padding={4}>Estampitiency</Flex>
         <section>
-          {products.map((product) => (
-            <Stack key={product.id}>
-              <Image src={product.image} />
-              <Stack flex={1}>
-                <Text fontSize="xl" fontWeight="500">{product.title}</Text>
-                <Text>{product.description}</Text>
+          {products.map((product) => {
+            const cartItem = cart.find(item => item.product.id === product.id)
+            return (
+              <Stack key={product.id}>
+                <Image src={product.image} />
+                <Stack flex={1}>
+                  <Text fontSize="xl" fontWeight="500">{product.title}</Text>
+                  <Text>{product.description}</Text>
+                </Stack>
+                {cartItem 
+                  ? (
+                    <Stack direction="row">
+                      <Button onClick={() => handleDecrement(product.id)} colorScheme="primary"> - </Button>
+                      <Text flex={1} fontSize="2xl" textAlign="center">{cartItem.count}</Text>
+                      <Button onClick={() => handleIncrement(product.id)} colorScheme="primary"> + </Button>
+                    </Stack>
+                ) : (
+                  <Button onClick={() => handleAdd(product)} colorScheme="primary">Agregar</Button>
+                )}
               </Stack>
-              <Button colorScheme="primary">Agregar</Button>
-            </Stack>
-          ))}
+              )
+          })}
         </section>
         <Flex bottom={0} paddingBottom={4} margin="auto" position="sticky" width="fit-content">
           <Button colorScheme="primary" size="lg" >3 productos (total: $12)</Button>
